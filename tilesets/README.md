87c79f514bea032550bc64eb4e715b1398c385c9
rohrsben <rohrsben@gmail.com>
2025-11-14 13:49:24 -0500

    feat: add a demo tileset

---
+++ tilesets/README.md
@@ -0,0 +1,3 @@
+Some tilesets to try out wavewall with.
+
+- testing: this was the simple tileset I made while, well, testing the program during development. Not very well made, but I figured it should be immortalized.
---
+++ tilesets/testing/tileset.lua
@@ -0,0 +1,18 @@
+return {
+    pseudotiles = {
+        corner = {
+            corner_90 = "90",
+        }
+    },
+    recipes = {
+        original = {
+            tiles = { "cross", "corner", "jump" },
+        },
+        corners = {
+            tiles = {
+                corner = 95,
+                corner_90 = 5
+            }
+        },
+    },
+}
