f391b9ecfb320b2f4680394b678b35fbbf2ca987
rohrsben <rohrsben@gmail.com>
2025-11-14 01:01:17 -0500

    prerelease-2025-11-13.14

--- flake.nix
+++ flake.nix
@@ -2,28 +2,41 @@
     inputs = {
         nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
         rust-overlay.url = "github:oxalica/rust-overlay";
-        flake-utils.url = "github:numtide/flake-utils";
     };
 
-    outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
-        flake-utils.lib.eachDefaultSystem (system:
+    outputs = { self, ... }@inputs:
         let
-            overlays = [ (import rust-overlay) ];
-            pkgs = import nixpkgs {
+            overlays = [ (import inputs.rust-overlay) ];
+            system = "x86_64-linux";
+            pkgs = import inputs.nixpkgs {
                 inherit system overlays;
             };
+            manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
         in {
-            packages."x86_64-linux" = 
-            let
-                manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
-            in
-                pkgs.rustPlatform.buildRustPackage {
-                    pname = "wavewall";
-                    version = "0.1.0";
+            packages.x86_64-linux = rec {
+                wavewall = pkgs.rustPlatform.buildRustPackage {
+                    pname = manifest.name;
+                    version = manifest.version;
                     cargoLock.lockFile = ./Cargo.lock;
-                    src = pkgs.lib.cleanSource ./.;
+                    src = pkgs.lib.fileset.toSource {
+                        root = ./.;
+                        fileset = pkgs.lib.fileset.intersection (pkgs.lib.fileset.fromSource (pkgs.lib.sources.cleanSource ./.)) (
+                            pkgs.lib.fileset.unions [
+                                ./.cargo
+                                ./src
+                                ./Cargo.toml
+                                ./Cargo.lock
+                            ]
+                        );
+                    };
+
+                    strictDeps = true;
+                };
+
+                default = wavewall;
             };
-            devShells.default = pkgs.mkShell {
+
+            devShells.x86_64-linux.default = pkgs.mkShell {
                 buildInputs = with pkgs; [
                     rust-bin.beta.latest.default
                     rust-analyzer
@@ -31,6 +44,5 @@
                     nodejs
                 ];
             };
-        }
-    );
+        };
 }
